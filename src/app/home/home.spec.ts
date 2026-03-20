ba97d2f5796832b03666db129d33e3af2c04b257
jarod2548 <jarodkonijnenberg@gmail.com>
2026-03-20 13:39:52 +0100

    unit test fixes, fake component injection

--- src/app/home/home.spec.ts
+++ src/app/home/home.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Home } from './home';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('Home', () => {
   let component: Home;
@@ -8,7 +10,17 @@ describe('Home', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Home]
+      imports: [Home],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ id: 123 }),
+            queryParams: of({ q: 'test' }),
+            snapshot: { paramMap: { get: (key: string) => '123' } },
+          },
+        },
+      ],
     })
     .compileComponents();
 
