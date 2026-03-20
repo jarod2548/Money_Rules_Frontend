2260b053662f1f626d8ea46d93c5dc9b6a3cad88
jarod2548 <jarodkonijnenberg@gmail.com>
2026-03-20 10:57:06 +0100

    Toevoeging van testen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { maakTransactie } from './maakTransactie/maakTransactie';
 import { LeesTransacties } from './lees-transacties/lees-transacties';
+import { maakCategorie } from './maakCategorie/maakCategorie';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'maakTransactie', component: maakTransactie },
-    { path: 'leesTransacties', component: LeesTransacties}
+    { path: 'leesTransacties', component: LeesTransacties},
+    {path: 'maakCategorie', component: maakCategorie }
 ];
