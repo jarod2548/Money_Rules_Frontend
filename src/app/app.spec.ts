1b4da331c3c52e06481f5f45f58bf075767d1b56
jarod2548 <jarodkonijnenberg@gmail.com>
2026-04-17 11:57:35 +0200

    update

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,22 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { routes } from './app.routes';
 import { App } from './app';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, provideRouter } from '@angular/router';
 import { of } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
       providers: [
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            params: of({ id: 123 }),      // mock route params if you use them
-            queryParams: of({ q: 'test' }),
-            snapshot: { paramMap: { get: (key: string) => '123' } },
-          },
-        },
-      ],
+      provideRouter(routes),
+      provideHttpClient(),
+      importProvidersFrom(FormsModule),
+   ],
     }).compileComponents();
   });
 
