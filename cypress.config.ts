54381c50671ad23b71a1b6b2a37fda0eb6fdd1c8
jarod2548 <jarodkonijnenberg@gmail.com>
2026-04-19 19:24:42 +0200

    f

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,5 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://frontend:80",
-
-    setupNodeEvents(on, config) {
-      return config;
-    }
   }
 });
