1853cd6de00b160d83440a0c3423bb751e615a7d
jarod2548 <jarodkonijnenberg@gmail.com>
2026-04-19 18:53:48 +0200

    ik weet het niet meer

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from "cypress";
+const { defineConfig } = require("cypress");
 
-export default defineConfig({
-
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://frontend:80",
   },
 });
